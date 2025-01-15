0058556c519dd76278d5465a7f1cbbc5617fc843
Connor M <ConnorMontenegro00@gmail.com>
2025-01-15 22:16:48 +0000

    Commands with no arguments can be run from default path containing only /bin

--- aos_wish_shell/aos_wish_shell/wish.c
+++ aos_wish_shell/aos_wish_shell/wish.c
@@ -1,6 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_PATH 10
+
+char *path[MAX_PATH] = {"/bin"};
+
+void printPath() {
+  printf("Current path:\n");
+  for (int i = 0; path[i] != NULL; i++){
+    printf("%s\n", path[i]);
+  }
+}
+
+int runCmd(char *cmd) {
+
+  cmd[strcspn(cmd, "\n")] = 0;
+  pid_t pid;
+  int status;
+  char cmdPath[256];
+  
+  for (int i = 0; path[i] != NULL; i++){
+    strcpy(cmdPath, path[i]);
+    if (cmdPath[strlen(cmdPath) - 1] != '/') {
+      strcat(cmdPath, "/");
+    }
+    strcat(cmdPath, cmd);
+
+    pid = fork();
+
+    if (pid == 0){
+      execl(cmdPath, cmd, (char *)NULL);
+      perror("execl");
+      exit(EXIT_FAILURE);
+    } else if (pid < 0){
+      perror("fork");
+      return -1;
+    } else {
+      wait(NULL);
+      return 0;
+    }
+  }
+  return -1;
+}
 
 int main() {
   char *input = NULL;
@@ -8,7 +53,19 @@ int main() {
 
   printf("wish> ");
   while (getline(&input, &len, stdin) != -1) {
-    printf("You entered: %s", input);
+
+    input[strcspn(input, "\n")] = 0;
+    
+    if (strcmp(input, "exit") == 0){
+      printf("Exiting wish..\n");
+      break;
+    }
+    if (strcmp(input, "path") == 0){
+      printPath();
+      printf("wish> ");
+      continue;
+    }
+    runCmd(input);
     printf("wish> ");
   }
 
